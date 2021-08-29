c8e4ae0c808011c2ae2cb56fc533586fc73035ba
NoorMohamed-intern <NoorMohamed-intern>
2021-08-29 10:50:11 +0530

    api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,7 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 20;
 export const RES_PER_PAGE = 10;
-export const KEY = 'd592ed87-6930-4dbe-a435-f2b1397d2edc';
+export const KEY = 'f749ed12-c6f3-4b37-aca5-b4ddbf9f5edd';
+// export const KEY = 'd592ed87-6930-4dbe-a435-f2b1397d2edc';
 export const MODAL_CLOSE_SEC = 2.5;
 // export const API_KEY = 'b597abfe-ed6d-47e7-bd5c-d826cd0f4134';
